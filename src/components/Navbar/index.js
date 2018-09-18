04f99c2f7e36a588805d9765101422fd7e6b3d27
Joey Zheng <jzheng16@binghamton.edu>
2018-09-18 16:48:59 -0400

    feat: sticky navbar after certain scroll point

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import stickybits from 'stickybits';
 import './Navbar.css';
 import resume from '../../images/Zheng_Joey_Resume.pdf';
 
-export default props => (
-  <nav>
-    <ul className="nav-list">
+export default class NavbarContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleScroll = this.handleScroll.bind(this);
+  }
 
-      <li className="nav-item">
-        <a href="#top"> Home </a>
-      </li>
-      <li className="nav-item"><a href="#about"> About </a></li>
-      <li className="nav-item"><a href="#projects"> Projects </a></li>
-      <li className="nav-item"><a href="#contact"> Contact </a></li>
-      <li className="nav-item"><a href={resume} target="_blank" rel="noopener noreferrer">  Resume </a></li>
-    </ul>
-  </nav>
-);
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    this.setState({});
+  }
+
+  render() {
+    stickybits('#some-stickybit-nav');
+    return (
+      <nav id="some-stickybit-nav">
+        <ul className="nav-list sticky">
+
+          <li className="nav-item">
+            <a href="#top"> Home </a>
+          </li>
+          <li className="nav-item"><a href="#about"> About </a></li>
+          <li className="nav-item"><a href="#projects"> Projects </a></li>
+          <li className="nav-item"><a href="#contact"> Contact </a></li>
+          <li className="nav-item"><a href={resume} target="_blank" rel="noopener noreferrer">  Resume </a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Project.css';
+import pic1 from '../../images/bg2.jpg';
 
 export default () => (
   <section id="projects" className="projects">
     <h1> Projects  </h1>
     <div className="gallery">
-      <div className="card" id="project1">Project 1</div>
+      <div className="card" id="project1">
+        Project 1
+        <img className="project_picture" src={pic1} alt="Project 1" />
+      </div>
       <div className="card" id="project2">Project 2</div>
       <div className="card" id="project3">Project 3</div>
     </div>
