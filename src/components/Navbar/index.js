f9e6d9c4a36b09143b7617a8f218cdb0abb4c55d
Joey Zheng <jzheng16@binghamton.edu>
2018-09-17 23:58:10 -0400

    style: style landing image and created few animatons for images and buttons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './Navbar.css';
+import resume from '../../images/Zheng_Joey_Resume.pdf';
 
 export default props => (
   <nav>
     <ul className="nav-list">
+
       <li className="nav-item">
         <a href="#top"> Home </a>
       </li>
       <li className="nav-item"><a href="#about"> About </a></li>
       <li className="nav-item"><a href="#projects"> Projects </a></li>
       <li className="nav-item"><a href="#contact"> Contact </a></li>
-      <li className="nav-item"><a href="/src/images/resume.pdf" target="_blank" rel="noopener noreferrer">  Resume </a></li>
+      <li className="nav-item"><a href={resume} target="_blank" rel="noopener noreferrer">  Resume </a></li>
     </ul>
   </nav>
 );
