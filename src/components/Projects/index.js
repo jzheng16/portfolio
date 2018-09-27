7bffae6f9b53ec8bd98000d59262c0a84e5ed793
Joey Zheng <jzheng16@binghamton.edu>
2018-09-27 19:26:21 -0400

    style: added animations to headers, projects, and stack

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,9 +6,22 @@ import pic3 from '../../images/MFP1.png';
 
 export default () => (
   <section id="projects" className="projects">
-    <h1> Projects  </h1>
+    <h1
+      data-aos="fade-left"
+      data-aos-offset="200"
+      data-aos-delay="50"
+      data-aos-duration="1000"
+    >
+      Projects
+    </h1>
     <div className="gallery">
-      <div className="card">
+      <div
+        className="card"
+        data-aos="slide-right"
+        data-aos-offset="200"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+      >
         <div className="title">
           No Project Yet
           <span> Built using React/Redux </span>
@@ -16,20 +29,32 @@ export default () => (
         <a className="github" href="https://github.com/jzheng16/" target="_blank" rel="noopener noreferrer"> View Github </a>
         <img className="project_picture" id="project1" src={pic1} alt="Project 1" />
       </div>
-      <div className="card">
+      <div
+        className="card"
+        data-aos="slide-up"
+        data-aos-offset="200"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+      >
         <div className="title">
           REACTO deFacto
           <span> Built using React/Redux </span>
         </div>
-        <a className="github" href="https://github.com/jzheng16/" target="_blank" rel="noopener noreferrer"> View Github </a>
+        <a className="github" href="https://github.com/jmpressman/REACTO-platform" target="_blank" rel="noopener noreferrer"> View Github </a>
         <img className="project_picture" id="project2" src={pic2} alt="Project 2" />
       </div>
-      <div className="card">
+      <div
+        className="card"
+        data-aos="slide-left"
+        data-aos-offset="200"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+      >
         <div className="title">
           MyFitnessPal Clone
           <span> Built using React/Redux </span>
         </div>
-        <a className="github" href="https://github.com/jzheng16/" target="_blank" rel="noopener noreferrer"> View Github </a>
+        <a className="github" href="https://github.com/jzheng16/MFP-clone" target="_blank" rel="noopener noreferrer"> View Github </a>
         <img className="project_picture" id="project3" src={pic3} alt="Project 3" />
       </div>
     </div>
